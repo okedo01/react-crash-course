4df81222ce0a60eed590bb6d9d28d153b571349b
Eliah <eliahmwelangi01@gmail.com>
2024-11-10 21:55:51 -0800

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("hello, ninjas", e.target);
+    }
+
+const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+}
+
     return ( 
         <div className="home">
             <h1>Home page</h1>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={ (e) => handleClickAgain("mario", e) }>Click me again</button>
         </div>
      );
 }
