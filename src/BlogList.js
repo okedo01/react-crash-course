8f3f151dd999e17085d7afed125bdeb5daa5a869
Eliah <eliahmwelangi01@gmail.com>
2024-11-11 13:50:23 -0800

    Reusing_components

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,8 @@
 const BlogList = ({ blogs, title }) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
     
     return ( 
         <div className="blog-list">
-            <h1>All Blogs</h1>
+            <h1>{ title }</h1>
             { blogs.map(blog => (
                 <div className="blog-preview" key={blog.id}>
                      <h2>{ blog.title }</h2>
