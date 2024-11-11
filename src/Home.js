a14de196a0f325677defdc37b8379445323a189c
Eliah <eliahmwelangi01@gmail.com>
2024-11-11 14:21:01 -0800

    functions_as_props

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,15 @@ const Home = () => {
         {title: "Welcome party!", body: "lorem ipsum....", author: "Yoshi", id: 2},
         {title: "Web dev top tips", body: "lorem ipsum....", author: "Mario", id: 3}
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog  => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs = {blogs} title = "All Blogs"/>
-            <BlogList blogs = {blogs.filter((blog => blog.author === "Mario"))} title = "Marios's Blogs"/>
+            <BlogList blogs = {blogs} title = "All Blogs" handleDelete = { handleDelete }/>
         </div>
      );
 }
