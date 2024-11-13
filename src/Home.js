a7b806e3098ca0c907e337e1339fd21570bd8b8d
Eliah <eliahmwelangi01@gmail.com>
2024-11-13 06:49:04 -0800

    Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,25 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [ blogs, setBlogs ] = useState(null);
     const [ isPending, setIsPending ] = useState(true);
+    const [ error, setError] = useState(null);
 
     useEffect(() => {
         
         setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(response => response.json())
+            fetch('http://localhost:7000/blogs')
+            .then(response => {
+                if(!response.ok) {
+                    throw Error("404 Not Found");
+                }
+                return response.json();
+            })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(error => {
+                setError(error.message);
                 setIsPending(false);
             })
         }, 1000)
@@ -20,6 +31,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
+            { error && <div> { error } </div> }
             { isPending && <div> Loading...</div> }
             { blogs && <BlogList blogs = {blogs} title = "All Blogs" /> } 
         </div>
