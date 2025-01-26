96f20c0a448d556b3a47b38751ae14f25741c490
Eliah <eliahmwelangi01@gmail.com>
2025-01-26 11:05:26 +0300

    useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,25 +1,4 @@
-// import { useState } from "react";
-
-// const BlogList = ({blogs, title}) => {
-   
-//     return ( 
-//         <div className="blog-list">
-//             <h1>{ title }</h1>
-//             { blogs.map(blog => (
-//                 <div className="blog-preview" key={blog.id}>
-//                      <h2>{ blog.title }</h2>
-//                      <p>Written By: { blog.author }</p><br />
-//                 </div>
-//             ))}
-//         </div>
-//      );
-// }
- 
-// export default BlogList;
-
 const BlogList = ({blogs, title}) => {
-// const blogs = props.blogs;
-// const title = props.title;
 
     return (
         <div className="blog-list">
--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,31 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-// import BlogList from "./BlogList";
-// import useFetch from "./useFetch";
 
 const Home = () => {
-    // const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
-
-    // return ( 
-    //     <div className="home">
-    //         { error && <div> { error } </div> }
-    //         { isPending && <div> Loading...</div> }
-    //         { blogs && <BlogList blogs = {blogs} title = "All Blogs" /> } 
-    //     </div>
-    //  );
-
     const [blogs, setBlogs] = useState(null);
+    const [count, setCount] = useState(0);
+
+    // useEffect(() => {
+    //     fetch("http://localhost:8000/blogs")
+    //         .then(res => {
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             setBlogs(data);
+    //         })
+    // }, []);
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            })
-    }, []);
+        document.title = `Count: ${count}`;
+    }, [count]);
 
     return (
         <div className="container">
-           { blogs && <BlogList blogs={blogs} title="All blogs" /> }
+           {/* { blogs && <BlogList blogs={blogs} title="All blogs" /> } */}
+           <div className="count">
+            <button onClick={() => setCount(count + 1)}>count</button>
+            <p>counts: {count} </p>
+           </div>
         </div>
     );
 }
