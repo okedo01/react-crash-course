3de22eb9f5edf0b89e44e1c93df27dc28efa3efa
Eliah <eliahmwelangi01@gmail.com>
2025-01-27 09:22:35 +0300

    Using custom hooks

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,43 +1,31 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-    const [ data, setData ] = useState(null);
-    const [ isPending, setIsPending ] = useState(true);
-    const [ error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-
-        const abortCont = new AbortController();
-        
         setTimeout(() => {
-            fetch(url, { signal: abortCont.signal })
-            .then(response => {
-                if(!response.ok) {
-                    throw Error("404 Not Found");
-                }
-                return response.json();
-            })
-            .then(data => {
-                setData(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(error => {
-                if(error.name === "AbortError") {
-                    console.log("fetch aborted");
-                } else {
-                    setError(error.message);
-                    setIsPending(false);    
-                }
-            })
-        }, 1000)
+         fetch(url)
+         .then(res => {
+             if(!res.ok) {
+                 throw Error("Could not fetch data for that resource");
+             }
+             return res.json();
+         })
+         .then(data => {
+             setData(data);
+             setIsPending(false);
+             setError(null);
+         })
+         .catch(err => {
+             setError(err.message);
+             setIsPending(false);
+         })
+        }, 1000);
+ }, []);
 
-        return () => abortCont.abort();
-        
-    }, [url]);
-
-    return { data, isPending, error}
- 
+ return { data, isPending, error };
 }
-
 export default useFetch;
